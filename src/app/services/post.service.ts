a2078ccd4db100e02c6c40b3f978cdcdf826b5ec
Alejandro Pichola <g4alejandro29@gmail.com>
2022-01-21 15:28:22 -0600

    refactor called http

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PostInterface } from '@/app/interfaces/post';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,7 @@ export class PostService {
 
   constructor(private http: HttpClient) { }
 
-  async getPostArr ():Promise<Array<PostInterface>> {
-    return await this.http.get<Array<PostInterface>>('https://jsonplaceholder.typicode.com/posts2')
-    .toPromise()
+  getPostArr ():Observable<Array<PostInterface>> {
+    return this.http.get<Array<PostInterface>>('https://jsonplaceholder.typicode.com/posts2')
   }
 }
