6f2a7b54419dd78605ae89e906f4725c5b2776af
Maiver Alejandro Pichola Gua <61585@bi-central.local>
2022-02-16 16:34:03 -0600

    refactor(test): refactor config jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,21 +1,16 @@
-import type {Config} from '@jest/types';
+import type { Config } from '@jest/types';
 
 // Sync object
 const config: Config.InitialOptions = {
   verbose: true,
-    preset: "jest-preset-angular",
-    setupFilesAfterEnv: [
-      "<rootDir>/setupJest.ts"
-    ],
-    testPathIgnorePatterns: [
-      "<rootDir>/node_modules/",
-      "<rootDir>/dist/"
-    ],
-    globals: {
-      "ts-jest": {
-        "stringifyContentPathRegex": "\\.html$"
-      }
-    }
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setupJest.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  globals: {
+    'ts-jest': {
+      stringifyContentPathRegex: '\\.html$',
+    },
+  },
 };
 
 export default config;
