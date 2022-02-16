12fb697144261cb1675002e326faa9d3aebc9c85
Maiver Alejandro Pichola Gua <61585@bi-central.local>
2022-02-16 09:46:51 -0600

    feat(merge branch): adding config tailwind and primeng

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 const { guessProductionMode } = require('@ngneat/tailwind');
+const {banpais} = require('./tailwind/banpais');
+const {bancoIndustrial} = require('./tailwind/banco-industrial');
 
 process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
 
@@ -7,30 +9,8 @@ module.exports = {
     enabled: guessProductionMode(),
     content: ['./src/**/*.{html,ts}'],
   },
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          100: '#009DEB',
-        },
-        gray: {
-          250: '#D0D0D0',
-          550: '#848484',
-        },
-      },
-      fontSize: {
-        '28px': '1.75rem',
-      },
-      borderRadius: {
-        '5px': '0.313rem',
-      },
-      width: {
-        '50px': '3.125rem',
-      },
-      screens: {
-        xs: { min: '300px', max: '640px' },
-      },
-    },
-  },
+  theme: process.env.NG_APP_BANK === 'banco-industrial' ?
+    bancoIndustrial
+  : banpais,
   plugins: [],
 };
---
+++ tailwind/banpais.js
@@ -0,0 +1,27 @@
+const banpais = {
+  extend: {
+    colors: {
+      primary: {
+        100: '#f7d62b',
+      },
+      gray: {
+        250: '#D0D0D0',
+        550: '#848484',
+      },
+    },
+    fontSize: {
+      '28px': '1.75rem',
+    },
+    borderRadius: {
+      '5px': '0.313rem',
+    },
+    width: {
+      '50px': '3.125rem',
+    },
+    screens: {
+      xs: { min: '300px', max: '640px' },
+    },
+  },
+}
+
+module.exports = { banpais }
