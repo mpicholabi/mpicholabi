d09fa3e15a5d3764b74e7ad58917165586dcbdab
Maiver Alejandro Pichola Gua <61585@bi-central.local>
2022-02-23 11:13:22 -0600

    feat(config section benefit): config translation and test

---
+++ src/app/components/home/home-benefit/home-benefit.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeBenefitComponent } from './home-benefit.component';
+
+describe('HomeBenefitComponent', () => {
+  let component: HomeBenefitComponent;
+  let fixture: ComponentFixture<HomeBenefitComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ HomeBenefitComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomeBenefitComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
