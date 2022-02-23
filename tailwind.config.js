602c0b70661abbfdb58daaf4a509aa9d797b0fbb
Kirk Santizo <kisantizo@bi.com.gt>
2022-02-23 11:42:42 -0600

    feat(component card): card of benefit and add assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,48 @@ module.exports = {
     enabled: guessProductionMode(),
     content: ['./src/**/*.{html,ts}'],
   },
-  theme: process.env.NG_APP_BANK === 'banco-industrial' ?
-    bancoIndustrial
-  : banpais,
+  theme:
+  {
+    extend: {
+      colors: {
+        primary: {
+          100: '#009DEB',
+        },
+        gray: {
+          250: '#D0D0D0',
+          550: '#848484',
+        }
+      },
+      fontSize: {
+        '28px': '1.75rem',
+      },
+      borderRadius: {
+        '5px': '0.313rem',
+      },
+      width: {
+        '50px': '3.125rem',
+        '283px': '17.75rem',
+        '111px': '6.938rem'
+      },
+      screens: {
+        xs: { min: '300px', max: '640px' },
+      },
+      boxShadow: {
+        'card': '0px 3px 52px #9999992E'
+      },
+      borderRadius: {
+        '18px': '1.125rem'
+      },
+      padding: {
+        '37px': '2.313rem'
+      },
+      fontFamily: {
+        'lato': ['Lato', 'sans-serif']
+      },
+      fontSize:{
+        '22px': '1.375rem'
+      }
+    },
+  },
   plugins: [],
 };
