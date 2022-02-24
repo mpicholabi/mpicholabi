adfaf6bfb9f8dd085c193f4ec9574ea9cda6f197
Maiver Alejandro Pichola Gua <61585@bi-central.local>
2022-02-24 10:37:01 -0600

    feat(create section product): integration section product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { Store, select } from '@ngrx/store';
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { AppState } from '@/app/app.state';
-import { ErrorInterface } from './interfaces/error';
-import * as ErrorActions from '@/app/store/error.actions';
 import { TranslateService } from '@ngx-translate/core';
-import * as AOS from 'aos';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'AppTest';
-  ArrErrors: Array<ErrorInterface> = [];
 
   constructor(
     private store: Store<AppState>,
-    public translate: TranslateService
+    private translate: TranslateService,
   ) {
     this.translate.addLangs(['es']);
     this.translate.setDefaultLang('es');
-  }
-
-  generateErrorDummyVoid(): void {
-    const error: Array<ErrorInterface> = [{ code: 500, message: 'test'}]
-    this.store.dispatch(ErrorActions.ADD_ERROR({ payload: error }));
-  }
-
-  ngOnInit(): void {
-    AOS.init();
-    this.store.subscribe((state) => {
-      this.ArrErrors = state.errors;
-    });
   }
 }
