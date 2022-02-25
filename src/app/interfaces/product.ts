183603f19dc279c349259ce30fbaf67132913833
Kirk Santizo <kisantizo@bi.com.gt>
2022-02-25 15:03:30 -0600

    feat(create interfaces): create interface response, product and product detail

---
+++ src/app/interfaces/product.ts
@@ -0,0 +1,14 @@
+export interface ProductInterface<T>{
+  id: number,
+  code: string,
+  name: string,
+  description: string,
+  minLoanAmount: number,
+  maxLoanAmount: number,
+  minLoanInterest: number,
+  maxLoanInterest: number,
+  active: boolean,
+  startDate: string,
+  endDate: string,
+  details: T
+}
---
+++ src/app/interfaces/response-api.ts
@@ -0,0 +1,7 @@
+export interface responseApiInterface<T> {
+  data: T,
+  transactionId: string,
+  operationId: string,
+  statusCode: number,
+  transactionName: string
+}
