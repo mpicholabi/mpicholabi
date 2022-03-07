f67416beebe3ba81bbc6e8e70ddfcf420158e369
Kirk Santizo <kisantizo@bi.com.gt>
2022-03-07 15:35:05 -0600

    feat(create unit test services): config unit test services

--- src/app/services/product/product.service.spec.ts
+++ src/app/services/product/product.service.spec.ts
@@ -4,9 +4,14 @@ import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { dummyProduct } from './dummyProduct';
+
+const IsDetailed: boolean = true;
 
 describe('ProductService', () => {
   let service: ProductService;
+  let httpClient: HttpClient;
   let httpController: HttpTestingController;
 
   beforeEach(() => {
@@ -15,10 +20,33 @@ describe('ProductService', () => {
     });
 
     service = TestBed.inject(ProductService);
+    httpClient = TestBed.inject(HttpClient);
     httpController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('getProducts return product', () => {
+    const product = service.getProducts(IsDetailed);
+    expect(product).not.toBeNull();
+  });
+
+  it('Should call getProducts and return an object of posts', () => {
+    service.getProducts().subscribe((res) => {
+      expect(res).toEqual(dummyProduct);
+    });
+
+    const req = httpController.expectOne({
+      method: 'GET',
+      url: 'https://reqres.in/api/posts?IsDetailed=false',
+    });
+
+    req.flush(dummyProduct);
   });
 });
