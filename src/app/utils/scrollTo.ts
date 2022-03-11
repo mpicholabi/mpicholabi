03751f7fc89753eb17d5f976321998aa0ba2c632
Alejandro Picholá <mpichola@bi.com.gt>
2022-03-11 11:41:28 -0600

    fix(refactor scroll): repare issue of scroll in the document

--- src/app/utils/scrollTo.ts
+++ src/app/utils/scrollTo.ts
@@ -1,8 +1,11 @@
 export function scrollToTop(selector: string, offset: number = 0): void {
-    const element = document.querySelector(selector);
-    if (!element) return;
-    window.scrollTo({
-      top: element.getBoundingClientRect().top - offset,
-      behavior: 'smooth'
-    });
+  const element = document.querySelector(selector);
+  if (!element) return;
+  window.scrollTo({
+    behavior: 'smooth',
+    top:
+      element.getBoundingClientRect().top -
+      document.body.getBoundingClientRect().top -
+      offset,
+  });
 }
