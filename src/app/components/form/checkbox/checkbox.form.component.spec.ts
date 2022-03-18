9ca35a4fa243a64123d7479fa8dd5faf80e57413
Alejandro Picholá <mpichola@bi.com.gt>
2022-03-18 14:34:22 -0600

    feat(config unit test): add branch

--- src/app/components/form/checkbox/checkbox.form.component.spec.ts
+++ src/app/components/form/checkbox/checkbox.form.component.spec.ts
@@ -5,11 +5,13 @@ import { CheckboxFormComponent } from './checkbox.form.component';
 describe('CheckboxFormComponent when tested directly', () => {
   let component: CheckboxFormComponent;
   let fixture: ComponentFixture<CheckboxFormComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CheckboxFormComponent],
     });
     fixture = TestBed.createComponent(CheckboxFormComponent);
+    component = fixture.componentInstance;
   });
 
   it('Should create component', () => {
